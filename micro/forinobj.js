2031c712d0d98f5dd0739a9e9190ed192ec41852
Manuel Serrano <Manuel.Serrano@inria.fr>
2019-08-02 07:49:34 +0200

    .

--- micro/forinobj.js
+++ micro/forinobj.js
@@ -4,7 +4,7 @@
 function dyn( a, keys ) {
    let r = 0;
    for( let i = keys.length - 1; i >= 0; i-- ) {
-      r = a[ keys[ i ] ];
+      r += a[ keys[ i ] ];
    }
    return r;
 }
@@ -41,21 +41,26 @@ function loopSwt( n, a, b, ka, kb, acc ) {
    }
 }
 
-function test( n, loop ) { 
+function test( n, name ) { 
+   console.log( "testing", name );
    let r = 0;
    const k = n / 10;
    const a = {b: 3, c: 4, a: 1, z: 10, w: -1};
    const b = {b: 35, c: 47, x: -19, z: -1};
-   const ka = Object.keys( a );
-   const kb = Object.keys( b );
+   const na = Object.keys( a );
+   const nb = Object.keys( b );
    
    for( let i = 0; i < n; i++ ) {
       if( i % k === 0 ) console.log( i );
-      r = loop( 1000, a, b, ka, kb, 0 );
+      if( name === "dyn" ) {
+      	 r += loopDyn( 1000, a, b, na, nb, 0 );
+      } else {
+      	 r += loopSwt( 1000, a, b, na, nb, 0 );
+      }
    }
    return r;
 }
 
-test( 30000,  process.argv[ 2 ] === "dyn" ? loopDyn : loopSwt );
+test( 30000,  process.argv[ 2 ] || "dyn" );
 //console.log( "run=", t() );
    
