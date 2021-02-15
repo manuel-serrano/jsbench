d54f476fbe7cd3325e4b3d845605903b088e8b44
Manuel Serrano <Manuel.Serrano@inria.fr>
2021-02-15 17:20:34 +0100

    .

--- micro/ctor.js
+++ micro/ctor.js
@@ -9,26 +9,21 @@ function CTOR( a, b, c, d ) {
    this.b2 = b;
    this.c2 = c;
    this.d2 = d;
-   //   foo( this );
 }
 
 function test( n ) {
    let k = n / 10;
    let m = 0;
    var r = 0;
-   
-   
+   let j = 0;
+   let l = arr.length;
+  
    for( let i = 0; i < n; i++ ) {
       let III = i;
       if( III % k == 0 ) console.log( III );
-      if( i === n - 1 ) {
-	 Object.defineProperty( p, "d", {
-	    get: function() { return 333; },
-	    set: function( v ) {}
-	 } );
-      }
       let OOO = new CTOR( III, n, 0, 1 );
-      m += OOO.d;
+      if( ++j === l ) j = 0;
+      arr[ j ] = OOO;
    }
 
    return m;
@@ -43,12 +38,17 @@ const N = (process.argv[ 1 ] === "fprofile")
       ? 10000
       : (process.argv[ 2 ] ? parseInt( process.argv[ 2 ] ) : 70000000);
 
-console.log( "ctor(", N, ")..." );
+let arr = new Array( Math.min( N, 10 ) );
+
+console.log( "ctor2(", N, ")..." );
 
 console.log( "test=", test( N ) );
-
 var o2 = new CTOR( 10, 20, 30, 40 );
 
-console.log( o1 );
-console.log( o2, "d=", o2.d );
+let s = 0;
+for( let i = arr.length - 1; i >= 0; i-- ) {
+   s +=  ((arr[ i ].a > arr[ i ].b) ? 1 : -1);
+   s +=  ((arr[ i ].c2 > arr[ i ].d2) ? 1 : -1);
+}
 
+console.log( "s=", s );
