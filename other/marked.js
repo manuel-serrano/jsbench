6900cdb804da4e7757e987825f3e08a61655e2d6
Manuel Serrano <Manuel.Serrano@inria.fr>
2021-02-18 05:35:55 +0100

    marked

--- other/marked.js
+++ other/marked.js
@@ -2749,8 +2749,49 @@
 /*---------------------------------------------------------------------*/
 /*    testAll ...                                                      */
 /*---------------------------------------------------------------------*/
+const source = `# Marked
+
+[![npm](https://badgen.net/npm/v/marked)](https://www.npmjs.com/package/marked)
+[![gzip size](https://badgen.net/badgesize/gzip/https://cdn.jsdelivr.net/npm/marked/marked.min.js)](https://cdn.jsdelivr.net/npm/marked/marked.min.js)
+[![install size](https://badgen.net/packagephobia/install/marked)](https://packagephobia.now.sh/result?p=marked)
+[![downloads](https://badgen.net/npm/dt/marked)](https://www.npmjs.com/package/marked)
+[![github actions](https://github.com/markedjs/marked/workflows/Tests/badge.svg)](https://github.com/markedjs/marked/actions)
+[![snyk](https://snyk.io/test/npm/marked/badge.svg)](https://snyk.io/test/npm/marked)
+
+- built for speed
+- low-level compiler for parsing markdown without caching or blocking for long periods of time
+- light-weight while implementing all markdown features from the supported flavors & specifications
+- works in a browser, on a server, or from a command line interface (CLI)
+
+## Demo
+
+Checkout the [demo page](https://marked.js.org/demo/) to see marked in action
+
+## Docs
+
+Our [documentation pages](https://marked.js.org) are also rendered using marked
+Also read about:
+
+* [Options](https://marked.js.org/#/USING_ADVANCED.md)
+* [Extensibility](https://marked.js.org/#/USING_PRO.md)
+
+## Installation
+
+**CLI:** "npm install -g marked"
+
+**In-browser:** "npm install marked"
+
+## Usage
+
+### Warning: Marked does not [sanitize](https://marked.js.org/#/USING_ADVANCED.md#options) the output HTML. Please use a sanitize library, like [DOMPurify](https://github.com/cure53/DOMPurify) (recommended), [sanitize-html](https://github.com/apostrophecms/sanitize-html) or [insane](https://github.com/bevacqua/insane) on the output HTML!
+
+## License
+
+Copyright (c) 2011-2018, Christopher Jeffrey. (MIT License)
+`;
+					
 function testAll( verb ) {
-   const html = marked('# Marked in Node.js\n\nRendered by **marked**.');
+   const html = marked(source);
    
    return html;
 }
@@ -2772,9 +2813,11 @@ function main( bench, n ) {
    console.log( res );
 }
 
+const K = 20000;
+
 const N = 
    (process.argv[ 1 ] === "fprofile") 
-   ? 500
-   : process.argv[ 2 ] ? parseInt( process.argv[ 2 ] ) : 50000;
+   ? K / 10
+   : ((process.argv[ 2 ] ? parseInt( process.argv[ 2 ] ) : 1) * K);
 
 main( "marked", N ); 
