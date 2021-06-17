f51e7d054a18cfa48f565c1974d156f54ab9f6c3
Manuel Serrano <Manuel.Serrano@inria.fr>
2021-06-17 18:39:34 +0200

    .

---
+++ tools/engines/js78-prelude.js
@@ -0,0 +1,50 @@
+/*=====================================================================*/
+/*    .../prgm/project/hop/jsbench/tools/engines/js78-prelude.js       */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Sat Apr 15 07:38:36 2017                          */
+/*    Last change :  Thu Jun 17 18:35:58 2021 (serrano)                */
+/*    Copyright   :  2017-21 Manuel Serrano                            */
+/*    -------------------------------------------------------------    */
+/*    Prelude file for spidermonkey (js78)                             */
+/*=====================================================================*/
+"use strict";
+
+let exports = {};
+let module = { filename: "@PATH@" };
+const __dirname = "@DIRNAME@";
+
+let buffer_stdout = "";
+
+let process = {
+   argv: [ "@INTERPRETER@", "-" ].concat( scriptArgs ),
+   env: { HOME: "@HOME" },
+   stdout: {
+      write: function( n ) {
+	 if( n == "\n" ) {
+	    print( buffer_stdout );
+	    buffer_stdout = "";
+	 } else {
+	    buffer_stdout += n;
+	 }
+      }
+   }
+}
+
+const console = {
+   log: print
+}
+
+Array.prototype.fill = function( value, start, end ) {
+   if( !start ) start = 0;
+   if( !end ) end = this.length;
+   
+   for( let i = start; i < end; i++ ) {
+      this[ i ] = value;
+   }
+}
+      
+const global = {
+   console: console,
+   process: process
+}
