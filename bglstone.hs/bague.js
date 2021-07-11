e7617ce55b99c77437a86301c929ce99c167c83d
Manuel Serrano <Manuel.Serrano@inria.fr>
2021-07-11 09:14:04 +0200

    bglstone.hs

---
+++ bglstone.hs/bague.js
@@ -0,0 +1,166 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/jsbench/bglstone/bague.hs.js            */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Pierre Weis                                       */
+/*    Creation    :  Fri Apr  1 10:00:21 1994                          */
+/*    Last change :  Sat Jul 10 09:55:53 2021 (serrano)                */
+/*    -------------------------------------------------------------    */
+/*    Resolution recursive du Baguenaudier: bench les appels de        */
+/*    fonctions et les acces aux vecteurs                              */
+/*    avec 21 pierres le nombre de coups est 1398101                   */
+/*    avec 24 pierres le nombre de coups est 11184810                  */
+/*    f (n+1) = 2*f(n) + n mod 2 avec f 1 = 1                          */
+/*=====================================================================*/
+"use strict";
+"use hopscript";
+
+//import { Vector } from "hopscript";
+
+let nombre_de_coups = 0;
+let jeu/*:vector*/ = new Vector(0);
+
+const une_pierre = 1;
+const une_case_vide = 0;
+
+function init_jeu( nombre_de_pierres/*:integer*/ ) {
+   nombre_de_coups = 0;
+   jeu = new Vector( nombre_de_pierres );
+   
+   for( let i = nombre_de_pierres - 1; i >= 0; i-- ) {
+      jeu[ i ] = une_pierre;
+   }
+}
+
+function la_case( n/*:integer*/ ) {
+   return n - 1;
+}
+
+function enleve_la_pierre( n ) {
+   if( jeu[ la_case( n ) ] === une_pierre ) {
+      jeu[ la_case( n ) ] = une_case_vide;
+   }
+}
+
+function pose_la_pierre( n ) {
+   if( jeu[ la_case( n ) ] === une_case_vide ) {
+      jeu[ la_case( n ) ] = une_pierre;
+   }
+}
+
+function autorise_mouvement( n ) {
+   switch( n ) {
+      case 1:
+	 return true;
+      case 2:
+	 return jeu[ la_case( 1 ) ] === une_pierre;
+      default:
+	 if( jeu[ la_case( n - 1 ) ] !== une_pierre )
+	    return false;
+
+	 let b = true;
+
+	 for( let i = 0; i <= la_case( n - 2 ); i++ ) {
+	    b = b && (jeu[ i ] === une_case_vide);
+	    i++;
+	 }
+	 
+	 return b;
+   }
+}
+
+function enleve_pierre( n ) {
+   nombre_de_coups++;
+   if( autorise_mouvement( n ) ) {
+      enleve_la_pierre( n );
+   }
+}
+
+function pose_pierre( n ) {
+   nombre_de_coups++;
+   if( autorise_mouvement( n ) ) {
+      pose_la_pierre( n );
+   }
+}
+
+function run( nombre_de_pierres ) {
+   function bague( n ) {
+      switch( n ) {
+	 case 1:
+	    enleve_pierre( 1 );
+	    return;
+	    
+	 case 2:
+	    enleve_pierre( 2 );
+	    enleve_pierre( 1 );
+	    return;
+	    
+	 default:
+	    bague( n - 2 );
+	    enleve_pierre( n );
+	    repose( n - 2 );
+	    bague( n - 1 );
+      }
+   }
+
+   function repose( n ) {
+      switch( n ) {
+	 case 1:
+	    pose_pierre( 1 );
+	    return;
+
+	 case 2:
+	    pose_pierre( 1 );
+	    pose_pierre( 2 );
+	    return;
+
+	 default:
+	    repose( n - 1 );
+	    bague( n - 2 );
+	    pose_pierre( n );
+	    repose( n - 2 );
+      }
+   }
+
+   init_jeu( nombre_de_pierres );
+   bague( nombre_de_pierres );
+
+   let res = 0;
+
+   switch( nombre_de_pierres ) {
+      case 1: res = 1; break;
+      case 2: res = 2; break;
+      case 10: res = 682; break;
+      case 14: res = 10922; break;
+      case 20: res = 699050; break;
+      case 24: res = 11184810; break;
+      case 25: res = 22369621; break;
+      case 26: res = 44739242; break;
+      case 27: res = 89478485; break;
+      case 28: res = 178956970; break;
+   }
+
+   return "res=" + res + " nb-coups=" + nombre_de_coups;
+}
+   
+function main( bench, n ) {
+   let res = 0;
+   const k = Math.round( n / 10 );
+   let i = 1;
+   
+   console.log( bench + "(", n, ")..." );
+   
+   while( n-- > 0 ) {
+      if( n % k === 0 ) { console.log( i++ ); }
+      res = run( 25 );
+   }
+
+   console.log( "res=", res );
+}
+
+const N = 
+   (process.argv[ 1 ] === "fprofile") 
+   ? 4
+   : process.argv[ 2 ] ? parseInt( process.argv[ 2 ] ) : 40;
+
+main( "bague", N ); 
+
---
+++ bglstone.hs/qsort.js
@@ -0,0 +1,106 @@
+/*=====================================================================*/
+/*    serrano/prgm/project/hop/jsbench/bglstone/qsort.hs               */
+/*    -------------------------------------------------------------    */
+/*    Author      :  Manuel Serrano                                    */
+/*    Creation    :  Thu Feb  9 14:03:22 2017                          */
+/*    Last change :  Fri May  7 17:00:35 2021 (serrano)                */
+/*    Copyright   :  2017-21 Manuel Serrano                            */
+/*    -------------------------------------------------------------    */
+/*    Transcription of the Scheme qsort.scm benchmark.                 */
+/*=====================================================================*/
+"use hopscript";
+
+let seed:int = 0;
+
+function rand():int {
+   seed = ((seed * 25173) + 17431) & 4095;
+   return seed;
+}
+
+function qsort( lo:int, hi:int, a:vector ) {
+   if( lo < hi) {
+      let i = lo;
+      let j = hi;
+      let pivot = a[ hi ];
+
+      while( i < j ) {
+	 while( i < hi && a[ i ] <= pivot ) i++;
+	 while( j > lo && a[ j ] >= pivot ) j--;
+
+	 if( i < j ) {
+	    let temp = a[ i ];
+	    a[ i ] = a[ j ];
+	    a[ j ] = temp;
+	 }
+      }
+
+      {
+	 let temp = a[ i ];
+	 a[ i ] = a[ hi ];
+	 a[ hi ] = temp;
+      }
+
+      qsort( lo, i - 1, a );
+      qsort( i + 1, hi, a );
+   }
+}
+
+
+function test( size:int ) {
+   let a = new Vector( size );
+   let check = new Vector( 4096 );
+
+   a[ size - 1 ] = 0;
+   
+   for( let i = 0; i < check.length; i++ ) check[ i ] = 0;
+
+   for( let j = 0; j <= size - 1; j++ ) {
+      let n = rand();
+      a[ j ] = n;
+      check[ n ]++;
+   }
+
+   qsort( 0, size - 1, a );
+
+   check[ a[ 0 ] ]--;
+
+   for( let k = 1; k <= size - 1; k++ ) {
+      if( a[ k - 1 ] > a[ k ] ) {
+	 throw( "Illegal sort " + k );
+      } else {
+	 check[ a[ k ] ]--;
+      }
+   }
+
+   for( let m = 0; m <= 4095; m++ ) {
+      if( check[ m ] != 0 ) {
+	 throw "Illegal sort " + m;
+      }
+   }
+   
+   return true;
+}
+
+function main( bench, n ) {
+   const size = 1 << 16;
+   let res = 0;
+   const k = Math.round( n / 10 );
+   let i = 1;
+   
+   console.log( bench + "(", n, ")..." );
+   
+   while( n-- > 0 ) {
+      if( n % k === 0 ) { console.log( i++ ); }
+      res = test( size );
+   }
+
+   console.log( "res=", res );
+}
+
+const N:int = 
+   (process.argv[ 1 ] === "fprofile") 
+   ? 20
+   : process.argv[ 2 ] ? parseInt( process.argv[ 2 ] ) : 1000;
+
+main( "qsort", N ); 
+
