0ef261a3db9bd47b3814320b1a03a7c256c655e7
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-03-28 12:31:40 +0200

    NAPI test_date

---
+++ napi/test_date/test_date.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const test_date = require(`./build/Release/test_date`);
+
+let res = true;
+
+const assert = {
+   ok: v => res = res && v,
+   throws: proc => { try { proc(); res = false; } catch(e) { ; } },
+   strictEqual: (a, b) => { res = res && (a === b) || (Number.isNaN(a) && Number.isNaN(b)); },
+   notStrictEqual: (a, b) => { res = res && (a !== b); },
+   deepStrictEqual: (a, b) => { Object.keys(a).forEach(k => { res = res && (a[k] === b[k]); }) },
+   notDeepStrictEqual: (a, b) => !assert.deepStrictEqual(a, b)
+}
+
+// Testing api calls for number
+function testNumber(num) {
+  assert.strictEqual(num, test_number.Test(num));
+}
+
+function test(N) {
+   res = true;
+
+   for (let i = 0; i < N; i++) {
+      const dateTypeTestDate = test_date.createDate(1549183351);
+      assert.strictEqual(test_date.isDate(dateTypeTestDate), true);
+
+      assert.strictEqual(test_date.isDate(new Date(1549183351)), true);
+
+      assert.strictEqual(test_date.isDate(2.4), false);
+      assert.strictEqual(test_date.isDate('not a date'), false);
+      assert.strictEqual(test_date.isDate(undefined), false);
+      assert.strictEqual(test_date.isDate(null), false);
+      assert.strictEqual(test_date.isDate({}), false);
+
+      assert.strictEqual(test_date.getDateValue(new Date(1549183351)), 1549183351);
+   }
+   
+   return res;
+}
+
+function main(bench, n) {
+   const k = Math.round(n / 10);
+   let i = 1;
+   
+   console.log(bench + "(" + n + ")...");
+   
+   while (n-- > 0) {
+      if (n % k === 0) { console.log( i++ ); }
+      test(10000);
+   }
+
+   console.log("res=", res);
+}
+
+const N = 
+   (process.argv[ 1 ] === "fprofile") 
+   ? 2
+   : process.argv[ 2 ] ? parseInt(process.argv[ 2 ]) : 1000;
+
+main("test_number", N); 
