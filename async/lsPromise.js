736e222dfe900cbb6ccf111f416d271e0e2dff33
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-06-16 12:10:44 +0200

    Calibration

--- async/lsPromise.js
+++ async/lsPromise.js
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  manuel serrano                                    */
 /*    Creation    :  Sun Jan  8 13:54:48 2023                          */
-/*    Last change :  Fri Jun 16 11:48:20 2023 (serrano)                */
+/*    Last change :  Fri Jun 16 12:10:37 2023 (serrano)                */
 /*    Copyright   :  2023 manuel serrano                               */
 /*    -------------------------------------------------------------    */
 /*    LS implementation using the Node fs api.                         */
@@ -258,6 +258,6 @@ async function main(bench, n) {
 const N = 
    (process.argv[ 1 ] === "fprofile") 
    ? 2
-   : process.argv[ 2 ] ? parseInt(process.argv[ 2 ]) : 200;
+   : process.argv[ 2 ] ? parseInt(process.argv[ 2 ]) : 130;
 
 main("ls", N); 
